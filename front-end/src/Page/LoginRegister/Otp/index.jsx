b11e0c2dc1bfb0a0e39536806dce156516f75910
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-15 17:14:40 +0330

    update otp components

--- front-end/src/Page/LoginRegister/Otp/index.jsx
+++ front-end/src/Page/LoginRegister/Otp/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import {Stack} from '@mui/material'
+import { Button, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
+import OtpInput from "react-otp-input";
 export default function Otp() {
+  const [otp, setOtp] = useState("");
+  console.log(otp);
+
   return (
     <>
-    <Stack sx={{width:'500px',height:'500px',backgroundColor:'blue'}}>
-hbkbkn
-    </Stack>
+      <Stack sx={{backgroundColor:'#000416',width:'100%',height:'100vh',mx:'auto', alignContent:'center', justifyContent:'center', alignItems:'center'}}>
+        <Typography variant="h4" sx={{ marginBottom:'10px', color:'white' }}>Please Enter SMS Code</Typography>
+        <OtpInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={6}
+          renderSeparator={<span style={{color:'white',fontSize:'40px'}}>-</span>}
+          renderInput={(props) => <input {...props} />}
+          inputStyle={{padding:'10px', color:'black', fontSize:'20px'}}
+        />
+        <Button variant="contained" size="large" type="submit" sx={{width:'10%', marginTop:'10px' }} >Check Code</Button>
+      </Stack> 
     </>
-  )
+  );
 }
