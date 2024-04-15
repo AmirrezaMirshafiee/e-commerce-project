6adcc5fea9ae34b6a8a6c56de33a66aac49ba00a
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-15 13:48:05 +0330

    update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,16 @@
 import app from "./app.js";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config({path:'./config.env'})
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 const port = process.env.PORT || 7001;
-mongoose.connect(process.env.DATA_BASE).then(res=>{
-    console.log('database ic connoted')
-}).catch(err=>{
-    console.log(err)
-})
-app.listen(port,()=>{
-    console.log('server is run')
-})
+mongoose
+  .connect(process.env.DATA_BASE)
+  .then((res) => {
+    console.log("database ic connoted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(port, () => {
+  console.log("server is run");
+});
