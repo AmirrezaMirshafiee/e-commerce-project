8fb913c2b54441ba1f0d7bd8104b516ae557cb33
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-15 12:39:09 +0330

    update utils
    apiFeatures
    catchAsync
    catchError
    handleError

--- back-end/controllers/authCn.js
+++ back-end/controllers/authCn.js
@@ -1,7 +1,22 @@
+import User from "../Models/userModel.js";
+import bcrypt from "bcryptjs";
+import HandleError from "../Utils/handleError.js";
+import jwt from "jsonwebtoken";
 export const signUp = async (req, res, next) => {
   try {
+    const { password, ...others } = req.body;
+    const newPassword = bcrypt.hashSync(password, 10);
+    const newUser = await User.create({ ...others, password: newPassword });
+    const newCart=await Cart.create({userId :newUser._id,products:[]})
+    return res.status(201).json({
+      status: "success",
+      message: "register successfully",
+    });
   } catch (err) {
-
+    return res.status(200).json({
+      status: "failed",
+      message: err.message,
+    });
   }
 };
 export const signIn = async (req, res, next) => {
