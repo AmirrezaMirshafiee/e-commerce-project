907f7684f57663e9afaaf8aabd937c80b3df97f8
AmirrezMirshafiee <amirrezamirshafiee0@gmail.com>
2024-04-15 13:49:23 +0330

    Update auth controllers

--- back-end/controllers/authCn.js
+++ back-end/controllers/authCn.js
@@ -3,7 +3,7 @@ import User from "../Models/userModel.js";
 import bcrypt from "bcryptjs";
 import HandleError from "../Utils/handleError.js";
 import jwt from "jsonwebtoken";
-import Cart from '../models/cartModel.js';
+import Cart from "../models/cartModel.js";
 export const signIn = catchAsync(async (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
@@ -25,12 +25,12 @@ export const signIn = catchAsync(async (req, res, next) => {
       Footer: "informatics website",
     }),
     headers: {
-        'Content-Type':'application/json',
+      "Content-Type": "application/json",
       ApiKey: process.env.SMS_KEY,
     },
   });
   const dataMessage = await sendMessage.json();
-  console.log(dataMessage)
+  console.log(dataMessage);
   if (dataMessage.success) {
     return res.status(200).json({
       status: "success",
@@ -38,9 +38,9 @@ export const signIn = catchAsync(async (req, res, next) => {
     });
   } else {
     return res.status(400).json({
-        status: "failed",
-        message: "sms not sent",
-      });
+      status: "failed",
+      message: "sms not sent",
+    });
   }
 });
 
@@ -48,7 +48,7 @@ export const signUp = catchAsync(async (req, res, next) => {
   const { password, ...others } = req.body;
   const newPassword = bcrypt.hashSync(password, 10);
   const newUser = await User.create({ ...others, password: newPassword });
-  const newCart=await Cart.create({userId :newUser._id,products:[]})
+  const newCart = await Cart.create({ userId: newUser._id, products: [] });
   return res.status(201).json({
     status: "success",
     message: "register successfully",
@@ -56,38 +56,37 @@ export const signUp = catchAsync(async (req, res, next) => {
 });
 
 export const otp = catchAsync(async (req, res, next) => {
-    const {code,phone}=req.body;
-    const  user=await User.findOne({ phone })
-    const  isValidCode = await fetch('https://api.limosms.com/api/checkcode',{
-        method:'POST',
-        headers:{
-            ApiKey:process.env.SMS_KEY,
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({
-            Mobile:phone,
-            code
-        })
-    })
-    const messageRes=await isValidCode.json()
-    if (messageRes.success) {
-        const token = jwt.sign(
-            { id: user._id, role: user.role },
-            process.env.JWT_SECRET
-          );
-        return res.status(200).json({
-          status: "success",
-          data:{
-            token,
-            user:user.username
-          },
-          message: "login successfully",
-        });
-      } else {
-        return res.status(400).json({
-            status: "failed",
-            message: " wrong code",
-          });
-      }
-  
+  const { code, phone } = req.body;
+  const user = await User.findOne({ phone });
+  const isValidCode = await fetch("https://api.limosms.com/api/checkcode", {
+    method: "POST",
+    headers: {
+      ApiKey: process.env.SMS_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      Mobile: phone,
+      code,
+    }),
+  });
+  const messageRes = await isValidCode.json();
+  if (messageRes.success) {
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET
+    );
+    return res.status(200).json({
+      status: "success",
+      data: {
+        token,
+        user: user.username,
+      },
+      message: "login successfully",
+    });
+  } else {
+    return res.status(400).json({
+      status: "failed",
+      message: " wrong code",
+    });
+  }
 });
